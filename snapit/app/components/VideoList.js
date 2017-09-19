2d9a0df37d35bcb7db1ebfe02824fe6503414506
Gareth Dsouza <gdsouza@usc.edu>
2017-09-18 19:16:22 -0700

    Added listview touchableHighlights and images

--- snapit/app/components/VideoList.js
+++ snapit/app/components/VideoList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, ListView, StyleSheet, Text } from 'react-native';
+import { Image, View, ListView, StyleSheet, Text, TouchableHighlight
+ } from 'react-native';
 // import { List, ListItem } from 'react-native-elements'
 //
 // // import VideoListItem from "./video_list_item";
@@ -48,9 +49,19 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
   },
-  textComponent: {
-      fontSize: 24
-  }
+  style_row_view: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 50,
+    backgroundColor: '#FFFFFF',
+  },
+  style_text: {
+    flex: 1,
+    marginLeft: 12,
+    fontSize: 16,
+    color: '#333333',
+    alignSelf: 'center',
+  },
 
 });
 
@@ -60,17 +71,36 @@ class VideoList extends React.Component {
         this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     }
 
+    _renderRow(rowData, sectionID, rowID) {
+        console.log('render row ...');
+        return (
+            <TouchableHighlight onPress={this._onPressRow.bind(this.rowID, rowData)}>
+                <View style={styles.style_row_view}>
+                    <Image
+                        style={{width: 50, height: 50}}
+                        source={{uri: rowData.snippet.thumbnails.default.url}}
+                    />
+                    <Text style={styles.style_text}>{rowData.snippet.title}</Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+
+    _onPressRow(rowID, rowData) {
+        console.log(rowID, rowData);
+    }
+
     render () {
         const dataSource = this.ds.cloneWithRows(this.props.videos);
-
-      return (
-        <List>
-          <ListView
-            dataSource={dataSource}
-            renderRow={(rowData) => <Text style={styles.textComponent}> {rowData.etag} </Text>}
-          />
-        </List>
-      )
+        console.log(dataSource);
+        return (
+            <List>
+                <ListView
+                dataSource={dataSource}
+                renderRow = {this._renderRow.bind(this)}
+                />
+            </List>
+        )
     }
 }
 
